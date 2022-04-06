a2f7e5df00441ff43c6a558f2e426fa847ef5a6a
SebaKrk <s.sciuba@gmail.com>
2022-04-06 09:08:29 +0200

    remove lunchsreen

--- YourBooks/Controller/BooksTableViewController.swift
+++ YourBooks/Controller/BooksTableViewController.swift
@@ -30,6 +30,7 @@ class BooksTableViewController : UIViewController {
         setupView()
         setupNavigationBar()
         setupTableView()
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
